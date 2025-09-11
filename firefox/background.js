4d15d5263ec2ca47c0a841938420420e2e2cb305
Katie Vedder <kvedder@gmail.com>
2025-09-11 16:10:37 -0700

    add a firefox dir

---
+++ firefox/background.js
@@ -0,0 +1,65 @@
+// ORIGINAL LICENSE
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// With some modifications by https://github.com/kavedder
+// IDK do what you want 
+
+import { baseUrls, SUBMIT_REQUEST, ARCHIVE_CACHED, WAYBACK_CACHED } from './urls.js';
+
+// Add a listener to create the initial context menu items,
+// context menu items only need to be created at runtime.onInstalled
+browser.runtime.onInstalled.addListener(async () => {
+  for (const [baseUrl, archiveMode] of Object.entries(baseUrls)) {
+    chrome.contextMenus.create({
+      id: baseUrl,
+      title: archiveMode,
+      type: 'normal',
+      contexts: ['selection']
+    });
+  }
+});
+
+// Open a new search tab when the user clicks a context menu
+browser.contextMenus.onClicked.addListener((item, tab) => {
+  const refUrl = item.linkUrl;
+  const baseUrl = item.menuItemId;
+  const reqType = baseUrls[baseUrl];
+  let url = new URL(`https://${baseUrl}/`);
+  if (reqType == ARCHIVE_CACHED || reqType == WAYBACK_CACHED) {
+    url = new URL(`https://${baseUrl}/${refUrl}`);
+  } else if (reqType == SUBMIT_REQUEST) {
+    url.searchParams.set('url', refUrl);
+  }
+  
+  browser.tabs.create({ url: url.href, index: tab.index + 1 });
+});
+
+// Add or removes the request type from context menu
+// when the user checks or unchecks the request type in the popup
+storage.local.onChanged.addListener(({ enabledUrls }) => {
+  if (typeof enabledUrls === 'undefined') return;
+
+  const allUrls = Object.keys(baseUrls);
+  const currentUrls = new Set(enabledUrls.newValue);
+  const oldUrls = new Set(enabledUrls.oldValue ?? allUrls);
+  const changes = allUrls.map((baseUrl) => ({
+    baseUrl: baseUrl,
+    added: currentUrls.has(baseUrl) && !oldUrls.has(baseUrl),
+    removed: !currentUrls.has(baseUrl) && oldUrls.has(baseUrl)
+  }));
+
+  for (const { baseUrl, added, removed } of changes) {
+    if (added) {
+      browser.contextMenus.create({
+        id: baseUrl,
+        title: baseUrls[baseUrl],
+        type: 'normal',
+        contexts: ['selection']
+      });
+    } else if (removed) {
+      browser.contextMenus.remove(baseUrl);
+    }
+  }
+});
---
+++ firefox/urls.js
@@ -0,0 +1,11 @@
+// https://archive.is/https://www.nytimes.com/2025/09/03/us/florida-vaccine-mandates.html
+// https://archive.is/submit/?url=https%3A%2F%2Fwww.asldkfjalksdjflkajsdlfkjasldf.com
+export const ARCHIVE_CACHED = 'Fetch archive.is cached versions';
+export const SUBMIT_REQUEST = 'Submit archive.is request';
+export const WAYBACK_CACHED = 'Fetch latest Wayback Machine cached version';
+
+export const baseUrls = {
+  'archive.is': ARCHIVE_CACHED,
+  'archive.is/submit': SUBMIT_REQUEST,
+  'web.archive.org/web': WAYBACK_CACHED
+};
